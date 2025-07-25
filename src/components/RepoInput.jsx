51cfe96825983daf02ecea44b7795f0e4e986df1
Ayush-alt-sys <igayushacharya@gmail.com>
2025-07-25 22:46:40 +0530

    Code update

--- src/components/RepoInput.jsx
+++ src/components/RepoInput.jsx
@@ -1,7 +1,25 @@
-export default function RepoInput() {
+export default function RepoInput({ setRepo }) {
+   
+    const submit = (event) =>{
+        event.preventDefault();
+    }
+    
     return (
-        <main>
-            Repo Input By user
+        <main className="border border-[#1f2937] rounded-lg  h-auto p-10 m-8">
+            <h1 className="text-lg mb-1">Repository URL or Path</h1>
+            <form className="flex items-center justify-around w-full gap-10" onSubmit={submit}>
+                <div id="repo-name" className="flex flex-col gap-4">
+                    <input type="text" className="border border-[#1f2937] p-4 w-[30vw] bg-[#272e36] rounded-lg text-xl" 
+                    placeholder="github.com/owner/repo"
+                    onChange={(e)=> setRepo(e.target.value)}
+                    required
+                    />
+                </div>
+                <button id="statisticsBtn" type="submit" className="w-max h-fit p-3.5 text-xl font-semibold flex items-center gap-2 rounded-lg group cursor-pointer">
+                    <span>Get Statistics</span>
+                    <i className="fa-solid fa-arrow-right group-hover:translate-x-2 transition-all duration-300"></i>
+                </button>
+            </form>            
         </main>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Info from "../components/Info"
 import RepoInput from "../components/RepoInput"
+import RepoResult from "./RepoResult";
 
 export default function Home() {
+    const [repo, setRepo] = useState("");
+
     return(
         <main className="w-full h-auto flex flex-col justify-center items-center gap-2 p-10">
             <Info />
-            <RepoInput />
+            <RepoInput setRepo={setRepo}/>
+            <RepoResult repo={repo} setRepo={setRepo}/>
         </main>
     )
 }
